34d585365aad23e0016104cb753fdda84e2a9076
Nirmal Patel <161806730+Nirmalspatel@users.noreply.github.com>
2025-04-03 18:39:11 +0530

    mongodb(atlas)

--- db.js
+++ db.js
@@ -5,7 +5,8 @@ const mongoURI = "mongodb://localhost:27017/inotebook"; // Use a specific databa
 const connectaTomongo = async () => {
     try {
         await mongoose.connect(mongoURI, {
-          
+           useNewUrlParser: true,
+            useUnifiedTopology: true
 
         });
         console.log("Connected to MongoDB successfully");
