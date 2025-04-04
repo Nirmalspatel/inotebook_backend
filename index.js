9b3c37e94bf58585bf735adeebc6215a873e2e2c
Nirmal Patel <161806730+Nirmalspatel@users.noreply.github.com>
2025-04-04 14:29:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.use(cors())
 app.use(express.json());    
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/note',require('./routes/note'));
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+}
+)
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`)
 })
